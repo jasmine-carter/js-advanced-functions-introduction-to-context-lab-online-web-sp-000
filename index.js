d5b6c695c25c1d669c47c695aaae891be342c704
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-12 00:23:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 function createEmployeeRecord(record) {
   let employee = {}
-  let employee.firstName = record[0]
-  let employee.familyName = record[1]
-  let employee.title = record[2]
-  let employee.payPerHour = record[3]
+   employee.firstName = record[0]
+   employee.familyName = record[1]
+   employee.title = record[2]
+   employee.payPerHour = record[3]
   return employee
 }
