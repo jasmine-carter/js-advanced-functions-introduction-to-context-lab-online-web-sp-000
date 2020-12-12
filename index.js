68531930d1d6be7e1ca93f27e47131a0ea89806c
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-12 00:45:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 // Your code here
 
 function createEmployeeRecord(record) {
-  let employee = {}
-   employee.firstName = record[0]
-   employee.familyName = record[1]
-   employee.title = record[2]
-   employee.payPerHour = record[3]
-   employee.timeInEvents = []
-   employee.timeOutEvents = []
+  let employee = {};
+   employee.firstName = record[0];
+   employee.familyName = record[1];
+   employee.title = record[2];
+   employee.payPerHour = record[3];
+   employee.timeInEvents = [];
+   employee.timeOutEvents = [];
   return employee
-}
+};
 
 function createEmployeeRecords(records) {
-  return records.map(createEmployeeRecord)
-}
+  return records.map(createEmployeeRecord);
+};
 
 function createTimeInEvent(employee, timeIn) {
   let newEvent = {type: "TimeIn", hour: parseInt(timeIn.substring(11, 15)), date: timeIn.substring(0,10)}
-    employee.timeInEvents.push(newEvent)
-    return employee
+    employee.timeInEvents.push(newEvent);
+    return employee;
+};
+
+function createTimeOutEvent(employee timeOut) {
+  let newEvent = {type: "TimeOut", hour: parseInt(timeOut.substring(11, 15)), date: timeOut.substring(0,10)}
+    employee.timeOutEvents.push(newEvent);
+    return employee;
 }
